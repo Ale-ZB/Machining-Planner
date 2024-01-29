830976b87c4280a024b8e0684aa5f4e7b320b4bd
João Pedro Gava Ribeiro <jpribeirogava@gmail.com>
2024-01-29 19:21:56 -0300

    Prévia da interface

---
+++ MachiningPlanner/src/main/java/com/mycompany/machiningplanner/MachiningPlanner.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.machiningplanner;
+
+/**
+ *
+ * @author Usuário
+ */
+public class MachiningPlanner {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
